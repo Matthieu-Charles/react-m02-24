fd35be96a0c6eb8249a022d9d587b458f2a7a79b
Matthieu Charles <matth.charles@protonmail.com>
2024-01-22 14:47:55 +0100

    wip: config

---
+++ restaurant-project/src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBV3VzoTiXzgREeImFA9sWtwZFD5S5RLdE",
+  authDomain: "restaurant-project-3c59b.firebaseapp.com",
+  projectId: "restaurant-project-3c59b",
+  storageBucket: "restaurant-project-3c59b.appspot.com",
+  messagingSenderId: "124382593461",
+  appId: "1:124382593461:web:18344300600c5a579dd9fc",
+  measurementId: "G-G7D67MP47P",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
